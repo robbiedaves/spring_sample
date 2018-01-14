39cbd5d2b29f386beb82703db36e382763951669
Robin Davies <robbiedaves@hotmail.com>
2018-01-14 17:00:24 +0000

    Add spring xml config example changes

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,17 +2,30 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    public CustomerServiceImpl() {
 
+    }
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    private CustomerRepository customerRepository;
     @Override
+
     public List<Customer> findAll() {
         return customerRepository.findAll();
     }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
 
 }
