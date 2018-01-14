4112510967fedeafc43cb8b12aebd0500aeaba01
Robin Davies <robbiedaves@hotmail.com>
2018-01-14 20:58:10 +0000

    add properties file demo

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,14 +1,26 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
+    // if wired via XML use a setter
+    //public void setDbUsername(String dbUsername) {
+    //    this.dbUsername = dbUsername;
+    //}
+
+
+
     @Override
     public List<Customer> findAll() {
+        System.out.println(dbUsername);
         List<Customer> customers = new ArrayList<>();
         Customer customer = new Customer();
         customer.setFirstname("Robin");
